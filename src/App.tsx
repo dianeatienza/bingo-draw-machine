43aa952a677cfa48432d75cb42f8ff1a55d47752
Lei A <ldcatienza@gmail.com>
2026-01-29 16:04:57 +0800

    Added color enhancements, purplish and white

--- src/App.tsx
+++ src/App.tsx
@@ -152,10 +152,10 @@ function App() {
   return (
     <div className="bingo-root">
       <header className="bingo-header">
-        <h1 className="bingo-title">BINGO HOST</h1>
-        <p className="bingo-subtitle">
+        <h1 className="bingo-title">Family Bingo</h1>
+        {/* <p className="bingo-subtitle">
           Large-screen friendly control panel for live games
-        </p>
+        </p> */}
       </header>
 
       <main className="bingo-layout">
